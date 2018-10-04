a3da8a7308b20990c856959cced292aa3d72e461
Syed Amir <getaamirhere@gmail.com>
2018-10-04 17:44:54 +0530

    First Commit

---
+++ ShoppingCart/js/controller.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+
+// the storeController contains two objects:
+// - store: contains the product list
+// - cart: the shopping cart object
+function storeController($scope, $routeParams, DataService) {
+
+    $scope.productsList=["Fruits","Vegetable","Grocery"];
+    // get store and cart from service
+    $scope.store = DataService.store;
+    $scope.cart = DataService.cart;
+
+    // apply changes when cart items change
+    $scope.cart.itemsChanged = function (e) {
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    }
+
+    // use routing to pick the selected product
+    if ($routeParams.productSku != null) {
+        $scope.product = $scope.store.getProduct($routeParams.productSku);
+    }
+}
+
+function productController($scope, $routeParams, DataService) {
+   $scope.store = DataService.store;
+   if($routeParams.productName != null)
+   {
+        $scope.selectedCommodityList = $scope.store.getProductList($routeParams.productName);
+   }
+}
